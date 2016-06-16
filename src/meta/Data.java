7cd59d46631c046efcb917584dc6fb2832cff276
Lukas <public@lrose.de>
2016-06-16 15:44:55 +0200

    Created Data Class

---
+++ src/meta/Data.java
@@ -0,0 +1,9 @@
+package meta;
+
+import java.io.Serializable;
+
+public class Data implements Serializable {
+	public static int xFields = 10;
+	public static int yFields = 10;
+	public static int mines = 10;
+}
