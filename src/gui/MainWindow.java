89632b437d659764ccf37d868a8c5fa33b8b853a
Lukas <public@lrose.de>
2016-06-16 15:42:26 +0200

    Moved local variables to fields

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -28,21 +28,33 @@ public class MainWindow extends Application {
 	GridPane menues, mainMenu, difficultyMenu;
 	Scene mainScene;
 	GamePane gp;
-	
-	public static void main(String args){
+	private Label difficultyLabel;
+	private Label nameLabel;
+	private ComboBox difficulty;
+	private TextField name;
+	private Button newGame;
+	private CheckBox proportional;
+	private Label yTilesLabel;
+	private Label xTilesLabel;
+	private Label minesLabel;
+	private Spinner<Integer> xTilesSpinner;
+	private Spinner<Integer> yTilesSpinner;
+	private Spinner<Integer> minesSpinner;
+
+	public static void main(String args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Minesweeper w/ JavaFX");
-//		AnchorPane aP = new AnchorPane();
+		// AnchorPane aP = new AnchorPane();
 		root = new BorderPane();
 		menues = new GridPane();
 		mainMenu = new GridPane();
 		difficultyMenu = new GridPane();
 		mainScene = new Scene(root, 1000, 1000);
-		
+
 		menues.setAlignment(Pos.CENTER);
 		menues.setHgap(10d);
 		menues.setVgap(10d);
@@ -53,56 +65,56 @@ public class MainWindow extends Application {
 		difficultyMenu.setHgap(10d);
 		difficultyMenu.setVgap(10d);
 		root.setPadding(new Insets(25, 25, 25, 25));
-		
-		Label difficultyLabel = new Label("Difficulty:");
-		Label nameLabel = new Label("Your name:");
-		
-		ComboBox difficulty = new ComboBox();
+
+		difficultyLabel = new Label("Difficulty:");
+		nameLabel = new Label("Your name:");
+
+		difficulty = new ComboBox();
 		difficulty.getItems().addAll("Easy", "Intermediate", "Hard", "Custom");
 		difficulty.setValue("Easy");
-		
-		TextField name = new TextField("Max Mustermann");
-		
-		Button newGame = new Button("New Game");
-		
-		CheckBox proportional = new CheckBox("Proportional");
-		
-		Label xTilesLabel = new Label ("Width:");
-		Label yTilesLabel = new Label ("Height:");
-		Label minesLabel = new Label ("Mines:");
-		
-		Spinner<Integer> xTilesSpinner = new Spinner<Integer>(1, 100, Constraints.xFields);
-		Spinner<Integer> yTilesSpinner = new Spinner<Integer>(1, 100, Constraints.yFields);
-		Spinner<Integer> minesSpinner = new Spinner<Integer>(1, 100, Constraints.mines);
-		
+
+		name = new TextField("Max Mustermann");
+
+		newGame = new Button("New Game");
+
+		proportional = new CheckBox("Proportional");
+
+		xTilesLabel = new Label("Width:");
+		yTilesLabel = new Label("Height:");
+		minesLabel = new Label("Mines:");
+
+		xTilesSpinner = new Spinner<Integer>(1, 100, Constraints.xFields);
+		yTilesSpinner = new Spinner<Integer>(1, 100, Constraints.yFields);
+		minesSpinner = new Spinner<Integer>(1, 100, Constraints.mines);
+
 		xTilesSpinner.setEditable(true);
 		yTilesSpinner.setEditable(true);
 		minesSpinner.setEditable(true);
-		
+
 		proportional.selectedProperty().addListener((ov, old, current) -> {
-			if (current){
+			if (current) {
 				yTilesSpinner.getValueFactory().setValue(Constraints.xFields);
 			}
 		});
-		
+
 		xTilesSpinner.valueProperty().addListener((ov, old, current) -> {
 			Constraints.xFields = xTilesSpinner.getValueFactory().getValue();
-			if (proportional.isSelected()){
+			if (proportional.isSelected()) {
 				yTilesSpinner.getValueFactory().setValue(Constraints.xFields);
 			}
 		});
-		
+
 		yTilesSpinner.valueProperty().addListener((ov, old, current) -> {
 			Constraints.yFields = yTilesSpinner.getValueFactory().getValue();
-			if (proportional.isSelected()){
+			if (proportional.isSelected()) {
 				xTilesSpinner.getValueFactory().setValue(Constraints.yFields);
 			}
 		});
-		
+
 		minesSpinner.valueProperty().addListener((ov, old, current) -> {
 			Constraints.mines = minesSpinner.getValueFactory().getValue();
 		});
-		
+
 		difficultyMenu.add(proportional, 0, 0);
 		difficultyMenu.add(xTilesLabel, 1, 0);
 		difficultyMenu.add(xTilesSpinner, 2, 0);
@@ -110,43 +122,47 @@ public class MainWindow extends Application {
 		difficultyMenu.add(yTilesSpinner, 4, 0);
 		difficultyMenu.add(minesLabel, 5, 0);
 		difficultyMenu.add(minesSpinner, 6, 0);
-		
+
 		difficultyMenu.setVisible(false);
-		
+
 		difficulty.valueProperty().addListener((ov, old, current) -> {
-			if (current.equals("Custom")){
+			if (current.equals("Custom")) {
 				difficultyMenu.setVisible(true);
 			} else {
 				difficultyMenu.setVisible(false);
 			}
 		});
-		
+
 		mainMenu.add(difficultyLabel, 0, 0);
 		mainMenu.add(difficulty, 1, 0);
 		mainMenu.add(nameLabel, 2, 0);
 		mainMenu.add(name, 3, 0);
 		mainMenu.add(newGame, 4, 0);
-		
+
 		menues.add(mainMenu, 0, 0, 1, 1);
 		menues.add(difficultyMenu, 0, 1, 1, 1);
 		root.setTop(menues);
-		
+
 		newGame.setOnAction((event) -> {
 			newGame();
 		});
-		
+
 		newGame();
-		
+
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
-	
-	public void newGame(){
-		if (gp == null){
+
+	public void newGame() {
+		if (gp == null) {
 			gp = new GamePane(Constraints.xFields, Constraints.yFields, Constraints.mines);
 		} else {
 			gp.newGame(Constraints.xFields, Constraints.yFields, Constraints.mines);
 		}
 		root.setCenter(gp);
+	}
+
+	public int getXTilesValue(){
+		return xTilesFa
 	}
 }
