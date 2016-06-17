f8977fcec7d2c033aed7c113f4ac5ad825a80d4f
Lukas Rose <public@lrose.de>
2016-06-17 11:03:47 +0200

    Update HighscoreEntry

--- src/meta/HighscoreEntry.java
+++ src/meta/HighscoreEntry.java
@@ -4,24 +4,45 @@ import java.io.Serializable;
 
 public class HighscoreEntry implements Serializable {
 	private String name;
-	private long time;
+	private long duration;
 	private long startTime;
+	
+	public HighscoreEntry(String name, long startTime, long duration){
+		this.name = name;
+		this.startTime = startTime;
+		this.duration = duration;
+	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public long getTime() {
-		return time;
+		return duration;
 	}
+	
 	public void setTime(long time) {
-		this.time = time;
+		this.duration = time;
 	}
+	
 	public long getStartTime() {
 		return startTime;
 	}
+	
 	public void setStartTime(long startTime) {
 		this.startTime = startTime;
+	}
+	
+	public boolean isEqual(Object anObject){
+		try{
+			HighscoreEntry another = (HighscoreEntry)anObject;
+			return another.getStartTime() == getStartTime();
+		} catch (Exception ex){
+			return false;
+		}
 	}
 }
