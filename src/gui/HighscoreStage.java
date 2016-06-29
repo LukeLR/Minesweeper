6de13d4fe524399a492cdae214230bb74a9102f4
Lukas <public@lrose.de>
2016-06-29 12:54:04 +0200

    New HighscoreStage created

---
+++ src/gui/HighscoreStage.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.time.LocalDateTime;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import meta.Data;
+import meta.DataManager;
+import meta.HighscoreEntry;
+import meta.HighscoreList;
+
+public class HighscoreStage extends Stage {
+	
+	public HighscoreStage(){
+		super.setTitle("Highscore");
+		
+	}
+	
+	
+}
