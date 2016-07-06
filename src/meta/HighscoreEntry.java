e75b42b82ae4567763f2b726f6735e5c928ce5ff
Lukas Rose <public@lrose.de>
2016-07-06 17:42:00 +0200

    Start of HighscoreEntry-ReWrite

--- src/meta/HighscoreEntry.java
+++ src/meta/HighscoreEntry.java
@@ -1,6 +1,7 @@
 package meta;
 
 import java.io.Serializable;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -13,144 +14,32 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class HighscoreEntry {
 	private SimpleStringProperty name;
-	private SimpleLongProperty duration;
-	private SimpleStringProperty durationString;
+	private SimpleIntegerProperty number, moves, fieldWidth, fieldHeight, mines;
 	private SimpleObjectProperty<LocalDateTime> startTime;
-	private SimpleStringProperty startTimeString;
-	private SimpleIntegerProperty moves;
-	private SimpleIntegerProperty xTiles;
-	private SimpleIntegerProperty yTiles;
-	private SimpleIntegerProperty mines;
+	private SimpleObjectProperty<Duration> duration;
 	
-	public HighscoreEntry(){
-		
-	}
-	
-	HighscoreEntry(String name, long startTime, long duration, int moves){
-		this.name = new SimpleStringProperty(name);
-		this.setStartTime(startTime);
-		this.setDuration(duration);
-		this.moves = new SimpleIntegerProperty(moves);
-		
-		this.startTime.addListener((ov, oldValue, newValue) -> {
-			startTimeString.set(formatStartTime());
-		});
-		
-		this.duration.addListener((ov, oldValue, newValue) -> {
-			durationString.set(formatDuration());
-		});
+	public HighscoreEntry(int number, String name, LocalDateTime startTime, Duration duration,
+                          int moves, int fieldWidth, int fieldHeight, int mines){
+		this.number = new SimpleIntegerProperty (number);
+		this.name = new SimpleStringProperty (name);
+		this.startTime = new SimpleObjectProperty<LocalDateTime> (startTime);
+		this.duration = new SimpleObjectProperty<Duration> (duration);
+		this.moves = new SimpleIntegerProperty (moves);
+		this.fieldWidth = new SimpleIntegerProperty (fieldWidth);
+		this.fieldHeight = new SimpleIntegerProperty (fieldHeight);
+		this.mines = new SimpleIntegerProperty (mines);
 	}
 	
-	public HighscoreEntry(String name, long startTime, long duration, int moves, int xTiles, int yTiles, int mines){
-		this(name, startTime, duration, moves);
-		this.xTiles = new SimpleIntegerProperty(xTiles);
-		this.yTiles = new SimpleIntegerProperty(yTiles);
-		this.mines = new SimpleIntegerProperty(mines);
+	public HighscoreEntry(String name, LocalDateTime startTime, Duration duration,
+            int moves, int fieldWidth, int fieldHeight, int mines){
+		this(0, name, startTime, duration, moves, fieldWidth, fieldHeight, mines);
 	}
 	
-	public String getName() {
-		return name.get();
+	public HighscoreEntry(int number, String name, LocalDateTime startTime, Duration duration, int moves){
+		this(number, name, startTime, duration, moves, 0, 0, 0);
 	}
 	
-	public void setName(String name) {
-		this.name.set(name);
-	}
-	
-	public long getDuration() {
-		return duration.get();
-	}
-	
-	public void setDuration(long duration) {
-		if (this.duration == null) this.duration = new SimpleLongProperty(duration);
-		else this.duration.set(duration);
-		if (this.durationString == null) this.durationString = new SimpleStringProperty(formatDuration());
-		else this.durationString.set(formatDuration());
-	}
-	
-	public LocalDateTime getStartTime() {
-		return startTime.get();
-	}
-	
-	public void setStartTime(long startTime) {
-		this.setStartTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(startTime), ZoneId.systemDefault()));
-	}
-	
-	public void setStartTime(LocalDateTime startTime){
-		if (this.startTime == null) this.startTime = new SimpleObjectProperty<LocalDateTime>(startTime);
-		else this.startTime.set(startTime);
-		if (this.startTimeString == null) this.startTimeString = new SimpleStringProperty(formatStartTime());
-		else this.startTimeString.set(formatStartTime());
-	}
-	
-	public boolean isEqual(Object anObject){
-		try{
-			HighscoreEntry another = (HighscoreEntry)anObject;
-			return another.getStartTime() == getStartTime();
-		} catch (Exception ex){
-			return false;
-		}
-	}
-	
-	public String formatStartTime(){
-		return getStartTime().getDayOfMonth() + "." + getStartTime().getMonth() + "." + getStartTime().getYear() + " " + getStartTime().getHour() + ":" + getStartTime().getMinute() + ":" + getStartTime().getSecond();
-	}
-	
-	public String formatDuration(){
-		int seconds = (int)((getDuration() % (1000 * 60))/1000);
-		int minutes = (int)((getDuration() % (1000 * 60 * 60))/(1000*60));
-		int millis = (int)(getDuration() % 1000);
-		return String.valueOf(minutes) + ":" + String.valueOf(seconds) + "." + String.valueOf(millis);
-	}
-
-	public int getxTiles() {
-		return xTiles.get();
-	}
-
-	public void setxTiles(int xTiles) {
-		this.xTiles.set(xTiles);
-	}
-
-	public int getyTiles() {
-		return yTiles.get();
-	}
-
-	public void setyTiles(int yTiles) {
-		this.yTiles.set(yTiles);
-	}
-
-	public int getMines() {
-		return mines.get();
-	}
-
-	public void setMines(int mines) {
-		this.mines.set(mines);
-	}
-
-	public int getMoves() {
-		return moves.get();
-	}
-
-	public void setMoves(int moves) {
-		this.moves.set(moves);
-	}
-	
-	public String getStartTimeString(){
-		/* Should produce the same output as formatStartTime(), but
-		 * is needed to autofill the highscore table by passing this
-		 * HighscoreEntry instance. The HighscoreTable will automatically
-		 * call these getters to fill it's values in the table cells.
-		 * Hence: DO NOT REMOVE!
-		 */
-		return startTimeString.get();
-	}
-	
-	public String getDurationString(){
-		/* Should produce the same output as formatStartTime(), but
-		 * is needed to autofill the highscore table by passing this
-		 * HighscoreEntry instance. The HighscoreTable will automatically
-		 * call these getters to fill it's values in the table cells.
-		 * Hence: DO NOT REMOVE!
-		 */
-		return durationString.get();
+	public HighscoreEntry(String name, LocalDateTime startTime, Duration duration, int moves){
+		this(0, name, startTime, duration, moves, 0, 0, 0);
 	}
 }
