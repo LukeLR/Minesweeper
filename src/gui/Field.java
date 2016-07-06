6f1a4f15d75573cfeb750921c68dd80200b872a7
Lukas Rose <public@lrose.de>
2016-07-06 22:46:26 +0200

    Felder, die diagonal liegen, ebenfalls öffnen, wenn sie keine Mine enthalten (habe ich entfernt da ich dachte, dass dies nicht gewollt ist -> ist es aber!)

--- src/gui/Field.java
+++ src/gui/Field.java
@@ -95,14 +95,14 @@ public class Field extends GridPane {
 				countNeighbourMines();
 				if (flagged) unflag();
 				if (neighbourMines == 0){
-//					if (tl != null && tl.isHidden()) tl.open();
+					if (tl != null && tl.isHidden()) tl.open();
 					if (tc != null && tc.isHidden()) tc.open();
-//					if (tr != null && tr.isHidden()) tr.open();
+					if (tr != null && tr.isHidden()) tr.open();
 					if (ml != null && ml.isHidden()) ml.open();
 					if (mr != null && mr.isHidden()) mr.open();
-//					if (bl != null && bl.isHidden()) bl.open();
+					if (bl != null && bl.isHidden()) bl.open();
 					if (bc != null && bc.isHidden()) bc.open();
-//					if (br != null && br.isHidden()) br.open();
+					if (br != null && br.isHidden()) br.open();
 				} else {
 					displayMineNum();
 				}
