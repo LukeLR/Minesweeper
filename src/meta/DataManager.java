05f967c7f1d19065af80593b1a0b1b6bda285613
Lukas Rose <public@lrose.de>
2016-07-06 22:18:26 +0200

    Updated DataManager to provide just one Data object<

--- src/meta/DataManager.java
+++ src/meta/DataManager.java
@@ -6,17 +6,11 @@ public class DataManager {
 	private static Data data;
 	private static int i = 0;
 	
-//	public static Data getData(){
-//		check();
-//		return data;
-//	}
-//	
-//	public static void setData(Data newData){
-//		data = newData;
-//	}
-	
 	private static void check(){
 		if (data == null){ data = new Data(); System.out.println("Created new data object (" + i + ")"); i++;}
 	}
-	
+
+	public static Data getData(){
+		check(); return data;
+	}
 }
