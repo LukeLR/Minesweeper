cc1d81e8f3ef6c15e684c35a176334d38399e560
Lukas Rose <public@lrose.de>
2016-07-06 16:33:17 +0200

    Implementation of HighscoreTableView

--- src/gui/HighscoreTableView.java
+++ src/gui/HighscoreTableView.java
@@ -3,19 +3,48 @@
  */
 package gui;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import meta.HighscoreList;
 
 public class HighscoreTableView<S> extends TableView<S> {
 	private HighscoreList list;
 	private boolean isCustomHighscoreTable = false;
+	private TableColumn number, name, startTime, duration, moves;
+	private TableColumn fieldWidth, fieldHeight, mines;
+	
 	public HighscoreTableView(){
+		this(null);
+	}
+	
+	public HighscoreTableView(HighscoreList list, boolean isCustomHighscoreTable){
 		super();
+		this.list = list;
+		this.isCustomHighscoreTable = isCustomHighscoreTable;
+		this.initColumns();
 	}
 	
 	public HighscoreTableView(HighscoreList list){
-		super();
-		this.list = list;
+		this(list, false);
+	}
+	
+	public HighscoreTableView(boolean isCustomHighscoreTable){
+		this(null, isCustomHighscoreTable);
+	}
+	
+	private void initColumns(){
+		number = new TableColumn("#");
+		name = new TableColumn("Name:");
+		startTime = new TableColumn("Start time:");
+		duration = new TableColumn("Duration:");
+		moves = new TableColumn("Moves:");
+		this.getColumns().addAll(number, name, startTime, duration, moves);
+		if (this.isCustomHighscoreTable){
+			fieldWidth = new TableColumn("Field width:");
+			fieldHeight = new TableColumn("Field heighth:");
+			mines = new TableColumn ("Mines:");
+			this.getColumns().addAll(fieldWidth, fieldHeight, mines);
+		}
 	}
 	
 	public void fillWithData(){
