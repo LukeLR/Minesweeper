bc2d131c7c84aa5f1e6631eeab5466b376364f17
Lukas Rose <public@lrose.de>
2016-07-06 19:48:13 +0200

    added missing addMove() and resetMoves() methods to DataManager

--- src/meta/DataManager.java
+++ src/meta/DataManager.java
@@ -91,4 +91,8 @@ public class DataManager {
 	public static void addHighscoreEntry(HighscoreEntry entry){check(); data.addHighscoreEntry(entry);}
 	
 	public static int getMoves(){check(); return data.getMoves();}
+	
+	public static void addMove(){check(); data.addMove();}
+	
+	public static void resetMoves(){check(); data.resetMoves();}
 }
