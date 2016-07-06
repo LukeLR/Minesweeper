07bac6b109b8052d94ec02cf5794c5eef30167cb
Lukas Rose <public@lrose.de>
2016-07-06 17:55:57 +0200

    Changes

--- src/gui/HighscoreTableView.java
+++ src/gui/HighscoreTableView.java
@@ -5,6 +5,7 @@ package gui;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import meta.HighscoreEntry;
 import meta.HighscoreList;
 
@@ -34,17 +35,28 @@ public class HighscoreTableView extends TableView<HighscoreEntry> {
 	}
 	
 	private void initColumns(){
-		number = new TableColumn("#");
-		name = new TableColumn("Name:");
-		startTime = new TableColumn("Start time:");
-		duration = new TableColumn("Duration:");
-		moves = new TableColumn("Moves:");
+		number = new TableColumn<HighscoreEntry, Integer>("#");
+		name = new TableColumn<HighscoreEntry, String>("Name:");
+		startTime = new TableColumn<HighscoreEntry, String>("Start time:");
+		duration = new TableColumn<HighscoreEntry, String>("Duration:");
+		moves = new TableColumn<HighscoreEntry, Integer>("Moves:");
 		this.getColumns().addAll(number, name, startTime, duration, moves);
 		if (this.isCustomHighscoreTable){
-			fieldWidth = new TableColumn("Field width:");
-			fieldHeight = new TableColumn("Field heighth:");
-			mines = new TableColumn ("Mines:");
+			fieldWidth = new TableColumn<HighscoreEntry, Integer>("Field width:");
+			fieldHeight = new TableColumn<HighscoreEntry, Integer>("Field heighth:");
+			mines = new TableColumn<HighscoreEntry, Integer> ("Mines:");
 			this.getColumns().addAll(fieldWidth, fieldHeight, mines);
+		}
+		
+		number.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("number"));
+		name.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("name"));
+		startTime.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("startTime"));
+		duration.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("duration"));
+		moves.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("moves"));
+		if (this.isCustomHighscoreTable){
+			fieldWidth.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("fieldWidth"));
+			fieldHeight.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("fieldHeight"));
+			mines.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("mines"));
 		}
 	}
 	
