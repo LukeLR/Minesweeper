8540ffbf2df69641873d99a10f68b2823b846bf1
Lukas Rose <public@lrose.de>
2016-07-08 00:03:03 +0200

    Prepared loading and saving

--- src/meta/DataManager.java
+++ src/meta/DataManager.java
@@ -5,6 +5,7 @@ import gui.MainWindow;
 public class DataManager {
 	private static Data data;
 	private static int i = 0;
+	public static String defaultPath = "savegame.dat";
 	
 	private static void check(){
 		if (data == null){ data = new Data(); System.out.println("Created new data object (" + i + ")"); i++;}
@@ -17,5 +18,21 @@ public class DataManager {
 	public static Data resetData(){
 		data = null;
 		return getData();
+	}
+	
+	public static void save(){
+		save(defaultPath);
+	}
+	
+	public static void save(String path){
+		
+	}
+	
+	public static void load(){
+		load(defaultPath);
+	}
+	
+	public static void load(String path){
+		
 	}
 }
