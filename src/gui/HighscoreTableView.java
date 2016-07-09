2bae3c96d97c6d363d6b89a9846ba821c2fdae6c
Lukas <public@lrose.de>
2016-07-09 17:06:17 +0200

    documentational comments

--- src/gui/HighscoreTableView.java
+++ src/gui/HighscoreTableView.java
@@ -52,13 +52,47 @@ public class HighscoreTableView extends TableView<HighscoreEntry> {
 			this.getColumns().addAll(fieldWidth, fieldHeight, mines);
 		}
 		
+		/*
+		 * The source code of the TableColumn class in OpenJDK declares a Cell Factory as follows:
+		 * 
+		 * public static final Callback<TableColumn<?,?>, TableCell<?,?>> DEFAULT_CELL_FACTORY = new Callback<TableColumn<?,?>, TableCell<?,?>>() {
+		 * 	@Override public TableCell<?,?> call(TableColumn<?,?> param) {
+		 * 		return new TableCell() {
+		 * 			@Override protected void updateItem(Object item, boolean empty) {
+		 * 				if (item == getItem()) return;
+		 * 
+		 * 				super.updateItem(item, empty);
+		 * 				if (item == null) {
+		 * 					super.setText(null);
+		 * 					super.setGraphic(null);
+		 * 				} else if (item instanceof Node) {
+		 * 					super.setText(null);
+		 * 					super.setGraphic((Node)item);
+		 * 				} else {
+		 * 					super.setText(item.toString());
+		 * 					super.setGraphic(null);
+		 * 				}
+		 * 			}
+		 * 		};
+		 * 	}
+		 * };
+		 * (http://hg.openjdk.java.net/openjfx/2.2/master/rt/file/e71070b0e0c0/javafx-ui-controls/src/javafx/scene/control/TableColumn.java)
+		 * 
+		 * This was taken as an inspiration on coding the Highscore row numbers as in the following lines:
+		 * 
+		 * Similiar problems are discussed in these StackOverflow questions:
+		 * http://stackoverflow.com/questions/19245822/custom-java-fx-cellfactory-messes-up-the-setcellvaluefactory
+		 * http://stackoverflow.com/questions/16384879/auto-numbered-table-rows-javafx
+		 * http://stackoverflow.com/questions/13449707/automatic-row-numbering-in-javafx-table and
+		 * http://stackoverflow.com/questions/10519534/cell-factory-in-javafx
+		 */
+		
 		number.setCellFactory(new Callback<TableColumn<HighscoreEntry, Integer>, TableCell<HighscoreEntry, Integer>>() {
 		    @Override public TableCell<HighscoreEntry, Integer> call(TableColumn<HighscoreEntry, Integer> param) {
 		        return new RowNumberCell<HighscoreEntry, Integer>();
 		    }
-		});
+		}); //TODO: Highscore Entry Numbers are wrong when the user changes the sorting of the tables (the values aren't sorted as well)
 		
-//		number.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, Integer>("number"));
 		name.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("name"));
 		startTime.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("startTime"));
 		duration.setCellValueFactory(new PropertyValueFactory<HighscoreEntry, String>("duration"));
