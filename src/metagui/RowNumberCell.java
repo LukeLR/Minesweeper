54eb0837f799703abc14a0a2a11bf63a52dc8ffb
Lukas <public@lrose.de>
2016-07-09 16:58:03 +0200

    Changed display of row numbers

--- src/metagui/RowNumberCell.java
+++ src/metagui/RowNumberCell.java
@@ -5,6 +5,6 @@ import javafx.scene.control.TableCell;
 public class RowNumberCell<S, T> extends TableCell<S, T> {
 	@Override
 	protected void updateItem(Object content, boolean empty){
-		this.setText(String.valueOf(this.getIndex()));
+		this.setText(String.valueOf(this.getIndex() + 1) + ".");
 	}
 }
